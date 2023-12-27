40f746aa6807f3edaba9915f9548a5978b42e015
Zac W <zacwalters4@gmail.com>
2023-12-26 20:59:09 -0700

    Add functionality to the search box

--- src/static/app.js
+++ src/static/app.js
@@ -5,31 +5,57 @@ if (document.readyState !== "loading") {
     document.addEventListener("DOMContentLoaded", setup)
 }
 
-// QUERY SELECTORS 
+// --------------- QUERY SELECTORS ---------------
 const productsContainer = document.querySelector(".products-container")
+const searchBox = document.querySelector('input[name="search"]')
 
-// DATA MODEL
+
+// --------------- EVENT LISTENERS ---------------
+
+
+// --------------- DATA MODEL ---------------
 
 let products
 
-// UTILITY FUNCTIONS
+// --------------- UTILITY FUNCTIONS ---------------
 
 async function setup() {
 	// Fetch the products data on page load
 	fetch('/products')
 	.then((response => response.json()))
 	.then((data) => {
-		products = data
-		products.forEach(product => createProductTile(product))
-	})
+		// Sort the products by price from low to high
+		data.sort((a, b) => a.price - b.price)
 
+		products = data
+
+		// Populate the products container with the newly fetched products
+		// products.forEach(product => createProductTile(product))
+		populateProducts()
+
+		searchBox.addEventListener('keyup', populateProducts)
+	})
 }
 
 const formatPrice = (price) => {
-	return price / 100
+	// Format price to dollars with cents shown
+	return (price / 100).toFixed(2)
 }
 
-// DOM UPDATING FUNCTIONS
+const filterProducts = () => {
+	// Grab search box value
+	const filter = searchBox.value
+
+	// Return the filtered array, use toLowerCase to make the search function case insensitive
+	return(products.filter((product) => product.title.toLowerCase().includes(filter.toLowerCase())))	
+}
+
+// --------------- DOM UPDATING FUNCTIONS ---------------
+
+const populateProducts = () => {
+	productsContainer.innerHTML = ''
+	filterProducts().forEach(product => createProductTile(product))
+}
 
 const createProductTile = (product) => {
 	productsContainer.innerHTML += 
