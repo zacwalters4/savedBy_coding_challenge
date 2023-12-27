22709146c4dd4fef2aae92d7f263690b4533c16c
Zac W <zacwalters4@gmail.com>
2023-12-26 20:03:52 -0700

    Create function to display products after they are fetched

--- src/static/app.js
+++ src/static/app.js
@@ -1,6 +1,39 @@
-window.addEventListener("DOMContentLoaded", setup);
+// check if document is already loaded, if it is already loaded, we don't need to listen for the event
+if (document.readyState !== "loading") {
+    setup()
+} else {
+    document.addEventListener("DOMContentLoaded", setup)
+}
+
+// QUERY SELECTORS 
+const productsContainer = document.querySelector(".products-container")
+
+// DATA MODEL
+
+let products
+
+// UTILITY FUNCTIONS
 
 async function setup() {
-	// START HERE
-	// PRODUCTS CAN BE FETCHED USING: GET /products
+	// Fetch the products data on page load
+	fetch('/products')
+	.then((response => response.json()))
+	.then((data) => {
+		products = data
+		products.forEach(product => createProductTile(product))
+	})
+
+}
+
+// DOM UPDATING FUNCTIONS
+
+const createProductTile = (product) => {
+	productsContainer.innerHTML += 
+		`
+        <div className="product-tile" key="${product.id}">
+          <img class="product-image" src="${product.images[0].src}" alt="${product.title} image"/>
+          <h3>${product.title}</h3>
+          <p>$${product.price}</p>
+        </div>
+		`
 }
